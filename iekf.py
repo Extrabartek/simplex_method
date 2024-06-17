810e1620ba02eab0c9d344c25b99be3e27eea34c
Bartosz Jemioł <b.s.jemiol@student.tudelft.nl>
2024-06-17 21:13:57 +0200

    Changes to matrix as output

--- iekf.py
+++ iekf.py
@@ -2,21 +2,21 @@ import numpy as np
 import math as m
 
 
-def state_dot(u: np.array):
-    return (np.array([[1, 0, 0],
+def state_dot_matrix(u: np.array):
+    return np.array([[1, 0, 0],
                       [0, 1, 0],
                       [0, 0, 1],
-                      [0, 0, 0]]) @ u).reshape(4, 1)
+                      [0, 0, 0]])
 
 
-def output(state: np.array):
+def output_matrix(state: np.array):
     return np.array([[m.atan2(state[2], state[0]) * (1 + state[3])],
                      [m.atan2(state[1], m.sqrt(state[0] ** 2 + state[2] ** 2))],
                      [m.sqrt(state[0] ** 2 + state[1] ** 2 + state[2] ** 2)], ])
 
 
-def output_diff(state: np.array, d_state: np.array):
-    return (np.array([[-state[2] * (state[3] + 1) / (state[0] ** 2 + state[2] ** 2), 0,
+def output_diff_matrix(state: np.array):
+    return np.array([[-state[2] * (state[3] + 1) / (state[0] ** 2 + state[2] ** 2), 0,
                        state[0] * (state[3] + 1) / (state[0] ** 2 + state[2] ** 2), m.atan2(state[2], state[0])],
                       [-state[0] * state[1] / (m.sqrt(state[0] ** 2 + state[2] ** 2) * (
                               state[0] ** 2 + state[1] ** 2 + state[2] ** 2)),
@@ -26,7 +26,7 @@ def output_diff(state: np.array, d_state: np.array):
                       [state[0] / m.sqrt(state[0] ** 2 + state[1] ** 2 + state[2] ** 2),
                        state[1] / m.sqrt(state[0] ** 2 + state[1] ** 2 + state[2] ** 2),
                        state[2] / m.sqrt(state[0] ** 2 + state[1] ** 2 + state[2] ** 2), 0]
-                      ]) @ d_state).reshape(3, 1)
+                      ])
 
 
-print(output_diff(np.array([1, 1, 1, 1]), np.array([1, 1, 1, 1])))
+print(output_diff_matrix(np.array([1, 1, 1, 1])))
